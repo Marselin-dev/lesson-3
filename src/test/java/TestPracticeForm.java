524bfa0b47147671a6f0a894332a562d88116093
marselinQA <avanesyan_sofia@mail.ru>
2026-02-16 13:11:48 +0300

    edits after a comment

--- src/test/java/TestPracticeForm.java
+++ src/test/java/TestPracticeForm.java
@@ -2,10 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.text;
 
@@ -13,24 +11,24 @@ import static com.codeborne.selenide.Condition.text;
 public class TestPracticeForm {
 
     @BeforeAll
-    static void beforeAll() {
+    static void setUp() {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.baseUrl= "https://demoqa.com";
         Configuration.timeout = 5000; // default 4000
     }
     @Test
     void checkPracticeForm(){
         //Есть проблемы с переходом на прямую по урлу https://demoqa.com/automation-practice-form была сделана такая альтернатива
-
-        open("https://demoqa.com/");
+        open("");
         $$(".card-body").findBy(text("Forms")).click();
-        $(byXpath("//li[@id='item-0']//span[text()='Practice Form']/..")).click();
+        $$ (".router-link").findBy(text("Practice Form")).click();
 
         //Заполняем данными форму practice-form
         $("#firstName").setValue("Poli");
         $("#lastName").setValue("Levyrv");
         $("#userEmail").setValue("wet@mail.ru");
-        $("[value=Female]").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("88005553535");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").$(byText("May")).click();
@@ -39,25 +37,26 @@ public class TestPracticeForm {
         $(".subjects-auto-complete__input").click();
         $("[aria-autocomplete=list").sendKeys("m");
         $(byText("Maths")).click();
-        $("#hobbies-checkbox-2").click();
-        $("#uploadPicture").uploadFile(new File("src/test/java/data/test.jpeg"));
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#uploadPicture").uploadFromClasspath("test.jpeg");
         $("#currentAddress").setValue("Seaview Avenue");
         $("#state").click();
-        $("#react-select-3-option-3").click();
+        $("#stateCity-wrapper").$(byText("Uttar Pradesh")).click();
         $("#city").click();
-        $("#react-select-4-option-0").click();
+        $("#stateCity-wrapper").$(byText("Agra")).click();
+
         $("#submit").click();
 
         //Проверки
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Student Name']]/td[2]")).shouldHave(text("Poli Levyrv"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Student Email']]/td[2]")).shouldHave(text("wet@mail.ru"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Gender']]/td[2]")).shouldHave(text("Female"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Mobile']]/td[2]")).shouldHave(text("8800555353"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Date of Birth']]/td[2]")).shouldHave(text("14 May,1999"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Subjects']]/td[2]")).shouldHave(text("Maths"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Hobbies']]/td[2]")).shouldHave(text("Reading"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Picture']]/td[2]")).shouldHave(text("test.jpeg"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='Address']]/td[2]")).shouldHave(text("Seaview Avenue"));
-        $(byXpath("//table[contains(@class, 'table-dark')]//tr[td[text()='State and City']]/td[2]")).shouldHave(text("Rajasthan Jaipur"));
+        $(".table-responsive").$(byText("Student Name")) .parent().shouldHave(text("Poli Levyrv"));
+        $(".table-responsive").$(byText("Student Email")) .parent().shouldHave(text("wet@mail.ru"));
+        $(".table-responsive").$(byText("Gender")) .parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")) .parent().shouldHave(text("8800555353"));
+        $(".table-responsive").$(byText("Date of Birth")) .parent().shouldHave(text("14 May,1999"));
+        $(".table-responsive").$(byText("Subjects")) .parent().shouldHave(text("Maths"));
+        $(".table-responsive").$(byText("Hobbies")) .parent().shouldHave(text("Reading"));
+        $(".table-responsive").$(byText("Picture")) .parent().shouldHave(text("test.jpeg"));
+        $(".table-responsive").$(byText("Address")) .parent().shouldHave(text("Seaview Avenue"));
+        $(".table-responsive").$(byText("State and City")) .parent().shouldHave(text("Uttar Pradesh Agra"));
     }
 }
